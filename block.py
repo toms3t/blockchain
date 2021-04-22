9bf6c9098ca9d70955f1d8ce597f1fdbdd3a5cc8
Tom <tomset@Air.local>
2021-04-22 11:10:38 -0400

    updated

--- block.py
+++ block.py
@@ -1,10 +1,13 @@
 from time import time
+from printable import Printable
 
-class Block:
+class Block(Printable):
     def __init__(self, index, previous_hash, transactions, proof, timestamp=None):
         self.index = index
         self.previous_hash = previous_hash
         self.transactions = transactions
         self.timestamp = time() if timestamp is None else timestamp
         self.proof = proof
+
+    
 
