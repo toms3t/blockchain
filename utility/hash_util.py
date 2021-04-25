c96b82ab0b58fff2ee70da12bd227580cf557eb1
Tom <tomset@Air.local>
2021-04-25 08:38:01 -0400

    working, just before web UI add

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -1,13 +1,20 @@
 import hashlib as hl
 import json
 
+# __all__ = ['hash_string_256', 'hash_block']
 
 def hash_string_256(string):
+    """Create a SHA256 hash for a given input string.
+
+    Arguments:
+        :string: The string which should be hashed.
+    """
     return hl.sha256(string).hexdigest()
 
+
 def hash_block(block):
-    """
-    Hashes a block and returns a string representation of it.
+    """Hashes a block and returns a string representation of it.
+
     Arguments:
         :block: The block that should be hashed.
     """
--- utility/verification.py
+++ utility/verification.py
@@ -1,7 +1,12 @@
-from hash_util import hash_string_256, hash_block
+"""Provides verification helper methods."""
+
+from utility.hash_util import hash_string_256, hash_block
+from wallet import Wallet
 
 class Verification:
-    def valid_proof(self, transactions, last_hash, proof):
+    """A helper class which offer various static and class-based verification and validation methods."""
+    @staticmethod
+    def valid_proof(transactions, last_hash, proof):
         """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
 
         Arguments:
@@ -17,30 +22,34 @@ class Verification:
         # Only a hash (which is based on the above inputs) which starts with two 0s is treated as valid
         # This condition is of course defined by you. You could also require 10 leading 0s - this would take significantly longer (and this allows you to control the speed at which new blocks can be added)
         return guess_hash[0:2] == '00'
-
-    def verify_chain(self, blockchain, get_balance):
+        
+    @classmethod
+    def verify_chain(cls, blockchain):
         """ Verify the current blockchain and return True if it's valid, False otherwise."""
         for (index, block) in enumerate(blockchain):
             if index == 0:
                 continue
             if block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
-            if not self.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
                 print('Proof of work is invalid')
                 return False
         return True
 
-    def verify_transaction(self, transaction, get_balance):
+    @staticmethod
+    def verify_transaction(transaction, get_balance, check_funds=True):
         """Verify a transaction by checking whether the sender has sufficient coins.
 
         Arguments:
             :transaction: The transaction that should be verified.
         """
-        sender_balance = get_balance()
-        return sender_balance >= transaction.amount
+        if check_funds:
+            sender_balance = get_balance()
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
 
-    def verify_transactions(self, open_transactions, get_balance):
+    @classmethod
+    def verify_transactions(cls, open_transactions, get_balance):
         """Verifies all open transactions."""
-        return all([self.verify_transaction(tx, get_balance) for tx in open_transactions])
-
-    
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
