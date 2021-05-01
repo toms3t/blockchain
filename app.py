be2a0f192c8a47dd5f54a3fd971d611252aa94a5
Tom <tomset@air.lan>
2021-05-01 09:41:51 -0400

    temp change

--- app.py
+++ app.py
@@ -247,14 +247,15 @@ def get_nodes():
     response = {"all_nodes": nodes}
     return jsonify(response), 200
 
+wallet = Wallet(80)
+blockchain = Blockchain(wallet.public_key, 80)
 
-if __name__ == "__main__":
-    from argparse import ArgumentParser
+# if __name__ == "__main__":
+#     from argparse import ArgumentParser
 
-    parser = ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=5000)
-    args = parser.parse_args()
-    port = args.port
-    wallet = Wallet(port)
-    blockchain = Blockchain(wallet.public_key, port)
-    app.run(host="0.0.0.0", port=port)
+#     parser = ArgumentParser()
+#     parser.add_argument("-p", "--port", type=int, default=5000)
+#     args = parser.parse_args()
+#     port = args.port
+    
+#     app.run(host="0.0.0.0", port=port)
